424fcd391a092f06a94281531613090ab64c4293
Paddy-Esmaeili <esmaep1@mcmaster.ca>
2025-01-22 08:52:50 -0500

    refactor: replaced all print statements with loggers

--- src/main/java/ca/mcmaster/se2aa4/mazerunner/Main.java
+++ src/main/java/ca/mcmaster/se2aa4/mazerunner/Main.java
@@ -1,11 +1,15 @@
 package ca.mcmaster.se2aa4.mazerunner;
 
-import org.apache.commons.cli.*;
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.DefaultParser;
+import org.apache.commons.cli.Options;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
 
@@ -21,10 +25,6 @@ public class Main {
     
         try {
             CommandLine cmd = parser.parse(options, args);
-            if(!cmd.hasOption("i")){
-                logger.error("Error: Missing the -i flag.");
-                return;
-            } 
             String inputFile = cmd.getOptionValue("i");
             logger.info("**** Reading the maze from file: {}", inputFile);
 
@@ -33,15 +33,15 @@ public class Main {
             while ((line = reader.readLine()) != null) {
                 for (int idx = 0; idx < line.length(); idx++) {
                     if (line.charAt(idx) == '#') {
-                        System.out.print("WALL ");
+                        logger.trace("WALL ");
                     } else if (line.charAt(idx) == ' ') {
-                        System.out.print("PASS ");
+                        logger.trace("PASS ");
                     }
                 }
-                logger.debug(mazeLine.toString());
+                logger.trace(System.lineSeparator());
             }
         } catch(Exception e) {
-            logger.error("/!\\ An error has occured /!\\", e);
+            logger.error("/!\\ An error has occured /!\\");
         }
         logger.info("**** Computing path");
         logger.warn("PATH NOT COMPUTED");
