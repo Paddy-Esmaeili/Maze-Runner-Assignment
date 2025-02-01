0d7cad7cd7edccc41f85c57fed16ada61f2db4eb
Paddy-Esmaeili <esmaep1@mcmaster.ca>
2025-02-01 12:14:31 -0500

    fix: Refactored the code so that it adheres to the Single Responsibility Principle and the Liskov Substitution Principle

--- src/main/java/ca/mcmaster/se2aa4/mazerunner/Main.java
+++ src/main/java/ca/mcmaster/se2aa4/mazerunner/Main.java
@@ -3,22 +3,16 @@ package ca.mcmaster.se2aa4.mazerunner;
 import org.apache.commons.cli.*;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.util.ArrayList;
-import java.util.List;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-
-   
     private static final Logger logger = LogManager.getLogger();
 
     public static void main(String[] args) {
-        logger.info("** Starting Maze Runner");
+        logger.info("** Starting Maze Runner **");
 
         Options options = new Options();
-        
         options.addOption("i", true, "Path to the maze file");
         options.addOption("p", true, "Validating user's input path");
 
@@ -29,86 +23,40 @@ public class Main {
             String validInput = cmd.getOptionValue("p");
 
             if (inputFile == null) {
-                throw new IllegalArgumentException("Error: i flag was not detected");
+                throw new IllegalArgumentException("Error: -i flag was not detected");
             }
 
-            Maze maze = Maze.loadMazeFromFile(inputFile);
+            Maze maze = new Maze(inputFile);
             logger.info("**** Reading the maze from file: {}", inputFile);
 
-            SimplePathGenerator simpleGenerator = new SimplePathGenerator();
-            String canonicalPath = simpleGenerator.findPath(maze);
+            PathGenerator generator = new RHRPathGenerator(maze);
+            logger.info("**** Computing path...");
+            String canonicalPath = generator.findPath();
+            logger.info("Canonical Path: {}", canonicalPath);
 
             if (validInput != null) {
                 logger.info("**** Validating path");
-                PathValidator validator = new PathValidator();
+                PathChecker validator = new PathValidator();
                 if (validator.checkPath(maze, validInput)) {
                     logger.info("correct path");
                 } else {
                     logger.info("incorrect path");
                 }
             }
-
-            logger.info("**** Computing path");
-            logger.info("Canonical Path: {}", canonicalPath);
-
         } catch (Exception e) {
             logger.error("An error occurred", e);
         }
 
-        logger.info("** End of Maze Runner");
+        logger.info("** End of Maze Runner **");
     }
 }
 
-class Maze {
-    private final char[][] grid;
-    private final int entryX, entryY, exitX, exitY;
+abstract class AbstractMaze {
+    protected char[][] grid;
+    protected int entryX = -1, entryY = -1, exitX = -1, exitY = -1;
 
-    public Maze(char[][] grid, int entryX, int entryY, int exitX, int exitY) {
+    public AbstractMaze(char[][] grid) {
         this.grid = grid;
-        this.entryX = entryX;
-        this.entryY = entryY;
-        this.exitX = exitX;
-        this.exitY = exitY;
-    }
-
-    public static Maze loadMazeFromFile(String inputFile) throws Exception {
-        List<char[]> tempGrid = new ArrayList<>();
-
-        try (BufferedReader reader = new BufferedReader(new FileReader(inputFile))) {
-            String line;
-            while ((line = reader.readLine()) != null) {
-                tempGrid.add(line.toCharArray());
-            }
-        }
-
-        char[][] grid = tempGrid.toArray(new char[0][]);
-        int entryX = -1, entryY = -1, exitX = -1, exitY = -1;
-
-        //detect entry points
-        for (int i = 0; i < grid.length; i++) {
-            if (grid[i][0] == ' ') {
-                entryX = i;
-                entryY = 0;
-                break;
-            }
-        }
-        
-        //detect exit points
-        for (int i = 0; i < grid.length; i++) {
-            if (grid[i][grid[i].length-1] == ' ') {
-                exitX = i;
-                exitY = grid.length-1;
-            }
-        }
-
-        if (entryX == -1 || entryY == -1) {
-            throw new IllegalArgumentException("Maze must have a valid entry point on the left side.");
-        }
-        if (exitX == -1 || exitY == -1) {
-            throw new IllegalArgumentException("Maze must have a valid exit point on the right side.");
-        }
-
-        return new Maze(grid, entryX, entryY, exitX, exitY);
     }
 
     public char[][] getGrid() {
@@ -131,112 +79,228 @@ class Maze {
         return exitY;
     }
 
-    //check where the walls are
+    public boolean isWall(int x, int y) {
+        return x < 0 || y < 0 || x >= grid.length || y >= grid[0].length || grid[x][y] == '#';
+    }
+}
+
+class Maze extends AbstractMaze {
+    public Maze(String inputFile) throws Exception {
+        super(loadGrid(inputFile));
+        findEntryExit();
+    }
+
+    private static char[][] loadGrid(String inputFile) throws IOException {
+        List<char[]> tempGrid = new ArrayList<>();
+        try (BufferedReader reader = new BufferedReader(new FileReader(inputFile))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                tempGrid.add(line.toCharArray());
+            }
+        }
+        if (tempGrid.isEmpty()) {
+            throw new IllegalArgumentException("Error: Maze file is empty or unreadable.");
+        }
+        return tempGrid.toArray(new char[0][]);
+    }
+
+    private void findEntryExit() {
+        for (int i = 0; i < grid.length; i++) {
+            if (grid[i][0] == ' ' && entryX==-1) {
+                entryX = i;
+                entryY = 0;
+            
+            }
+            if (grid[i][grid[0].length-1] == ' ' && exitX==-1) {
+                exitX = i;
+                exitY = grid[0].length-1;
+            }
+        }
+
+        System.out.println("entry points: " + "("+ entryX + "," + entryY+ ")");
+        System.out.println("entry points: " + "("+ exitX + "," + exitY+ ")");
+        
+        if (entryX == -1) {
+            throw new IllegalArgumentException("Maze must have a valid entry point on the left side.");
+        }
+        if (exitX == -1) {
+            throw new IllegalArgumentException("Maze must have a valid exit point on the right side.");
+        }
+
+    }
+}
+
+interface PathGenerator {
+    String findPath();
+}
+
+// Right-Hand Rule Path Generator with Loop Detection
+class RHRPathGenerator implements PathGenerator {
+    private int x, y;
+    private String direction = "right";
+    private final char[][] maze;
+    private final StringBuilder path;
+    private final Set<String> visited;
+    private final int exitX, exitY;
+
+    public RHRPathGenerator(AbstractMaze maze) {
+        this.maze = maze.getGrid();
+        this.x = maze.getEntryX();
+        this.y = maze.getEntryY();
+        this.exitX = maze.getExitX();   
+        this.exitY = maze.getExitY();
+        this.path = new StringBuilder();
+        this.visited = new HashSet<>();     //save the coordinates for cells that were already visited
+    }
+
+    public String findPath() {
+        while (!isExit(x, y)) {
+            String pos = x + "," + y;
+
+     
+            if (visited.contains(pos) && wallOnRight() && wallOnFront() && wallOnLeft()) {
+                break; 
+            }
+
+            visited.add(pos);
+
+            if (!wallOnRight()) {
+                turnRight();
+                if (!wallOnFront()) {  
+                    goForward();
+                }
+            } else if (!wallOnFront()) {
+                goForward();
+            } else {
+                turnLeft();
+            }
+        }
+        return path.toString();
+    }
+
+    private void goForward() {
+        path.append("F");
+        switch (direction) {
+            case "right" -> y++;
+            case "up" -> x--;
+            case "left" -> y--;
+            case "down" -> x++;
+        }
+    }
+
+    private void turnRight() {
+        path.append("R");
+        direction = switch (direction) {
+            case "right" -> "down";
+            case "down" -> "left";
+            case "left" -> "up";
+            case "up" -> "right";
+            default -> direction;
+        };
+    }
+
+    private void turnLeft() {
+        path.append("L");
+        direction = switch (direction) {
+            case "right" -> "up";
+            case "up" -> "left";
+            case "left" -> "down";
+            case "down" -> "right";
+            default -> direction;
+        };
+    }
+
+    private boolean wallOnFront() {
+        return !isValidMove(x, y, direction);
+    }
+
+    private boolean wallOnRight() {
+        return !isValidMove(x, y, getRightDirection(direction));
+    }
+
+    private boolean wallOnLeft() {
+        return !isValidMove(x, y, getLeftDirection(direction));
+    }
+
+    private boolean isValidMove(int x, int y, String direction) {
+        return switch (direction) {
+            case "right" -> !isWall(x, y + 1);
+            case "up" -> !isWall(x - 1, y);
+            case "left" -> !isWall(x, y - 1);
+            case "down" -> !isWall(x + 1, y);
+            default -> false;
+        };
+    }
+
+    private String getRightDirection(String currentDirection) {
+        return switch (currentDirection) {
+            case "right" -> "down";
+            case "down" -> "left";
+            case "left" -> "up";
+            case "up" -> "right";
+            default -> currentDirection;
+        };
+    }
+
+    private String getLeftDirection(String currentDirection) {
+        return switch (currentDirection) {
+            case "right" -> "up";
+            case "up" -> "left";
+            case "left" -> "down";
+            case "down" -> "right";
+            default -> currentDirection;
+        };
+    }
+
     public boolean isWall(int x, int y) {
         return grid[x][y] == '#';
     }
 
-    //check if it reached the exit point
-    public boolean isExit(int x, int y) {
+    private boolean isExit(int x, int y) {
         return x == exitX && y == exitY;
     }
 }
 
-class SimplePathGenerator {
-    public String findPath(Maze maze) {
-        StringBuilder path = new StringBuilder();
-        boolean[][] visited = new boolean[maze.getGrid().length][maze.getGrid()[0].length];   //cells that were visited before
-
-        int x = maze.getEntryX();
-        int y = maze.getEntryY();
-
-        if (findPathRecursive(maze, x, y, visited, path)) {
-            return path.toString();
-        } else {
-            System.out.println("Error: No possible paths were found.");
-            return "";    //return an empty string as the path
-        }
-    }
-
-    private boolean findPathRecursive(Maze maze, int x, int y, boolean[][] visited, StringBuilder path) {
-        if (maze.isExit(x, y)) {
-            return true;
-        }
-
-        visited[x][y] = true;
-
-        // Moving East
-        if (y + 1 < maze.getGrid()[x].length && !maze.isWall(x, y + 1) && !visited[x][y + 1]) {
-            path.append("F");
-            if (findPathRecursive(maze, x, y + 1, visited, path)) {
-                return true;
-            }
-
-            path.deleteCharAt(path.length() - 1);
-        }
-
-        //Moving South
-        if (x + 1 < maze.getGrid().length && !maze.isWall(x + 1, y) && !visited[x + 1][y]) {
-            path.append("R");
-            if (findPathRecursive(maze, x + 1, y, visited, path)) {
-                return true;
-            }
-            path.deleteCharAt(path.length() - 1);
-        }
-
-        // Moving North
-        if (x - 1 >= 0 && !maze.isWall(x - 1, y) && !visited[x - 1][y]) {
-            path.append("L");
-            if (findPathRecursive(maze, x - 1, y, visited, path)) {
-                return true;
-            }
-            path.deleteCharAt(path.length() - 1);
-        }
-
-        visited[x][y] = false;
-        return false;
-    }
+interface PathChecker {
+    boolean checkPath(AbstractMaze maze, String path);
 }
-class PathValidator {
-    public boolean checkPath(Maze maze, String path) {
+
+class PathValidator implements PathChecker {
+    public boolean checkPath(AbstractMaze maze, String path) {
         int x = maze.getEntryX();
         int y = maze.getEntryY();
+        String direction = "right";
 
         for (char move : path.toCharArray()) {
-            //The path cannot contain any characters other than F, R, and L
-            if (move != 'F' && move != 'R' && move != 'L') {
-                return false; 
-            }
-
-            // Move based on the user's input
             switch (move) {
-                case 'F': // Move forward 
-                    if (y + 1 < maze.getGrid()[x].length && !maze.isWall(x, y + 1)) {
-                        y++;
-                    } else {
-                        return false; // Hits a wall
+                case 'F' : {
+                    if (maze.isWall(x, y)) return false;
+                    switch (direction) {
+                        case "right" -> y++;
+                        case "up" -> x--;
+                        case "left" -> y--;
+                        case "down" -> x++;
                     }
-                    break;
-                case 'R': // Move right 
-                    if (x + 1 < maze.getGrid().length && !maze.isWall(x + 1, y)) {
-                        x++;
-                    } else {
-                        return false; // Hits a wall
-                    }
-                    break;
-                case 'L': // Move left 
-                    if (x - 1 >= 0 && !maze.isWall(x - 1, y)) {
-                        x--;
-                    } else {
-                        return false; // Hits a wall
-                    }
-                    break;
-                default:
-                    return false;   
+                }
+                case 'R' :
+                    direction = switch (direction) {
+                        case "right" -> "down";
+                        case "down" -> "left";
+                        case "left" -> "up";
+                        case "up" -> "right";
+                        default -> direction;
+                    };
+                case 'L' : 
+                    direction = switch (direction) {
+                        case "right" -> "up";
+                        case "up" -> "left";
+                        case "left" -> "down";
+                        case "down" -> "right";
+                        default -> direction;
+                    };
+                default : return false;
             }
         }
-        
-        //return true if the path hits no walls
-        return true;
+        return x == maze.getExitX() && y == maze.getExitY();
     }
 }
